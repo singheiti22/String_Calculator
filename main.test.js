7a8ac776c6813fb962d9ea0ba6800d6ad108590b
Eiti Singh <eiti.singh@ril.com>
2025-02-02 14:21:50 +0530

    Test: Add failing test for \n for new line

--- main.test.js
+++ main.test.js
@@ -15,3 +15,7 @@ test("should return sum of two comma-separated numbers", () => {
 test("should return sum of multiple comma-separated numbers", () => {
     expect(add("1,2,3,5,6,0")).toBe(17);
 })
+
+test("should handle \n for new line", () => {
+    expect(add("1\n2,3")).toBe(6);
+})
