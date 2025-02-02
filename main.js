32ff20089b8bd442c5110e37f93c2c67fc2aed30
Eiti Singh <eiti.singh@ril.com>
2025-02-02 14:34:06 +0530

    Feat: Handle negative numbers

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ function add(numbers) {
     if(numbers.length === 0) return 0;
 
     let delimiter = /,|\n/;
+
     if(numbers.startsWith('//')) {
         const parts = numbers.split('\n');
         delimiter = new RegExp(parts[0].slice(2));
         numbers = parts[1];
     }
+
     const numArray = numbers.split(delimiter).map(Number);
+
+    const negativeNumbers = numArray.filter(num => num < 0);
+
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negative numbers not allowed ${negativeNumbers.join(", ")}`);
+    }
+    
     return numArray.reduce((sum, num) => sum+num, 0);
 }
 
